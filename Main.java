c855c5d92f01be6ff449947553dc026819ff50c7
yigitalin <mustafayigitalin@icloud.com>
2026-02-19 15:25:13 +0300

    //testing1

--- Main.java
+++ Main.java
@@ -1222,10 +1222,149 @@ public class Main{
 }
 */
 //---------------------------------------------------------------------------------------------------------
+/*
+public class Main{
+    public static void main(String[] args){
+
+        //FOR LOOPS
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("pizza");
+        }
+        for(int x = 10; x > 0; x--){
+            System.out.println(x);
+        }
+        for(int y = 1; y <= 10; y+=2){
+            System.out.println(y);
+        }
+        for(int z = 10; z > 0; z-=2){
+            System.out.println(z);
+        }
+    }
+}
+*/
+/*
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
 
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for(int i = 0; i < max; i++){
+            System.out.println(i);
+        }
+        scanner.close();
     }
 }
+*/
+/*
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
+
+        for(int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        
+        System.out.println("HAPPY NEW YEAR!");
+        scanner.close();
+    }
+}
+*/
+//----------------------------------------------------------------------------------------------------
+/*
+public class Main{
+    public static void main(String[] args){
+
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 0; i < 10; i++){
+
+            if(i == 5){
+                break;
+            }
+
+            System.out.println(i + " ..");
+        }
+
+    }
+}
+*/
+/*
+public class Main{
+    public static void main(String[] args){
+
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 0; i < 10; i++){
+
+            if(i == 5){
+                continue;
+            }
+            
+            System.out.println(i + " ..");
+        }
+
+    }
+}
+*/
+//--------------------------------------------------------------------------------------------------
+/*
+public class Main{
+    public static void main(String[] args){
+
+        // NESTED LOOPS
+
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 9; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+/*
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the # of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the # of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter the symbol to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+//------------------------------------------------------------------------------------------------------

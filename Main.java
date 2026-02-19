2c543a3e9ce81ab28d38892478a605fdd6fbf325
yigitalin <mustafayigitalin@icloud.com>
2026-02-19 13:51:05 +0300

    //testing

--- Main.java
+++ Main.java
@@ -1222,3 +1222,10 @@ public class Main{
 }
 */
 //---------------------------------------------------------------------------------------------------------
+
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+    }
+}

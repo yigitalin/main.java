463b23d0c7302746792c9e0b3475103efbf5cb7e
yigitalin <mustafayigitalin@icloud.com>
2026-02-26 13:44:05 +0300

    //one

--- Main.java
+++ Main.java
@@ -1542,22 +1542,136 @@ public class Main{
     }
 }
 */
-//-----------------------------------------------------------------------------------
-
+//---------------------------------------------------------------------------------------------
+/*
 public class Main{
     public static void main(String[] args){
 
-        String pizza = bakePizza("flat bread", "mozzerella", "pepporoni");
+        //VARIABLES SCOPE
 
-        System.out.println(pizza);
+        int x = 1; //LOCAL
+        System.out.println(x);
+
+        doSomething();
     }
-    static String bakePizza(String bread){
-        return bread + " pizza";
+    static void doSomething(){
+        int x = 2; //LOCAL
+        System.out.println(x);
     }
-    static String bakePizza(String bread, String cheese){
-        return cheese + " " + bread + " pizza";
+}
+*/
+/*
+public class Main{
+
+    static int x = 3; //CLASS
+    public static void main(String[] args){
+
+        doSomething();
     }
-    static String bakePizza(String bread, String cheese, String topping){
-        return topping + " " + cheese + " " + bread + " pizza";
+    static void doSomething(){
+
+        System.out.println(x);
+    }
+}
+*/
+/*
+public class Main{
+
+    static int x = 3;
+    public static void main(String[] args){
+
+        int x = 1;
+        System.out.println(x);
+
+        doSomething();
+    }
+    static void doSomething(){
+        int x = 2;
+        System.out.println(x);
+    }
+}
+*/
+//----------------------------------------------------------------------------------------------------
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // JAVA BANKING PROGRAM FOR BEGINNERS
+
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        while(isRunning){
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
+
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch(choice){
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
+
+        System.out.println("***************************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("***************************");
+
+        scanner.close();
+    }
+
+    static void showBalance(double balance){
+        System.out.println("***************");
+        System.out.printf("$%.2f\n", balance);
+    }
+    static double deposit(){
+
+        double amount;
+
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if(amount < 0){
+            System.out.println("Amount can't be negative");
+            return 0;
+        }
+        else{
+            return amount;
+        }
+    }
+    static double withdraw(double balance){
+
+        double amount;
+
+        System.out.print("Enter amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if(amount > balance){
+            System.out.println("INSUFFICIENT FUNDS");
+            return 0;
+        }
+        else if(amount < 0){
+            System.out.println("Amount can't be negative");
+            return 0;
+        }
+        else{
+            return amount;
+        }
     }
 }
